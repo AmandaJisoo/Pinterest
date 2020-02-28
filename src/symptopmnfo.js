ce326a0538b009318ce7070ea839177fe1216c52
AMANDA JISOO PARK <wd1204@uw.edu>
2020-02-28 12:40:26 -0800

    import resolved

--- src/symptopmnfo.js
+++ src/symptopmnfo.js
@@ -1,8 +1,8 @@
-// import React, { Component } from 'react';
-// import { directive } from '@babel/types';
-// import { PinterestGrid, PinterestPinWidget } from './react-pinterest';
+import React, { Component } from 'react';
+import { directive } from '@babel/types';
+// import { PinterestGrid, PinterestPinWidget } from 'react-pinterest';
 
-// class SymptomInfo extends Component {
+// export class MyComponent extends React.Component {
 //     // In the constructor, set the initial state of search to empty string
 //     // Also *bind `this`* to the handleChange function
 //     constructor(props) {
@@ -12,9 +12,9 @@
 //     }
 //     render() {
 //         return (
-//             <PinterestGrid gutter={gutter}>
+//             <PinterestGrid gutter={2}>
 //             <PinterestPinWidget pin="530158187357124374" />
-//             <PinterestPinWidget pin="356417757989339525" />
+//             {/* <PinterestPinWidget pin="356417757989339525" />
 //             <PinterestPinWidget pin="356417757986524080" />
 //             <PinterestPinWidget pin="356417757986724718" />
 //             <PinterestPinWidget pin="356417757988564358" />
@@ -24,32 +24,11 @@
 //             <PinterestPinWidget pin="232850243203755319" />
 //             <PinterestPinWidget pin="137008013639035297" />
 //             <PinterestPinWidget pin="264797653065817757" />
-//             <PinterestPinWidget pin="144467100519920447" />
+//             <PinterestPinWidget pin="144467100519920447" /> */}
 //             </PinterestGrid>
 //         )
 //     } 
 // }  
 
 
-import React, { Component } from "react";
-import StackGrid from "react-stack-grid";
-
-export class MyComponent extends React.Component {
-  render() {
-    return (
-      <StackGrid
-        columnWidth={150}
-      >
-        <div key="key1">Item 1</div>
-        <div key="key2">Item 2</div>
-        <div key="key3">Item 3</div>
-        <div key="key4">Item 1</div>
-        <div key="key5">Item 2</div>
-        <div key="key6">Item 3</div>
-        <div key="key7">Item 1</div>
-        <div key="key8">Item 2</div>
-        <div key="key9">Item 3</div>
-      </StackGrid>
-    );
-  }
-}
+// import React, { Component } from "react";
