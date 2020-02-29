efee1ff2dce6d7fb8ce936cabe3d51055a143bd0
AmandaJisoo <apark8988@gmail.com>
2020-02-29 03:09:02 -0800

    last commit

--- src/Cards/Cards.js
+++ src/Cards/Cards.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
 import Card from './CardUI';
 import img1 from '../assets/index.jpg';
 import StackGrid from "react-stack-grid";
@@ -12,14 +12,18 @@ class Cards extends Component {
     }
 
     create = () => {
+
         let Data = [
             ["https://cdn.pixabay.com/photo/2017/06/12/19/02/cat-2396473__480.jpg", 
                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."],
             ["https://cdn.pixabay.com/photo/2015/06/03/13/13/cats-796437__480.jpg", 
                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."],
-            ["https://cdn.pixabay.com/photo/2012/11/26/13/58/cat-67345__480.jpg",, 
+            ["https://cdn.pixabay.com/photo/2012/11/26/13/58/cat-67345__480.jpg",
+                   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."],
+            ["https://cdn.pixabay.com/photo/2012/11/26/13/58/cat-67345__480.jpg",
                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."]
         ]
+
         // console.log(Data.length);
         let ele = [];
         for (let i = 0; i < Data.length; i++) {
@@ -40,11 +44,11 @@ class Cards extends Component {
         } = this.props;
 
         return(
-            <div>
-            <StackGrid columnWidth={width <= 500 ? '40%' : '30%'}>
+            // <div>
+            <StackGrid columnWidth={width <= 500 ? '30%' : '40%'}>
                 {this.create()}
             </StackGrid>
-            </div>
+            // </div>
         );
     }
 }
